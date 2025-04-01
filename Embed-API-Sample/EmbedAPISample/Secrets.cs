08632146b9786c9f6cdb4370e6f12fd06820664c
Ernesto Cianciotta <ernesto.cianciotta@microsoft.com>
2025-04-01 12:32:14 +0200

    First commit

---
+++ Embed-API-Sample/EmbedAPISample/Secrets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbedAPISample
+{
+    public class Secrets
+    {
+        // The Azure AD App - Application ID/Client ID
+        public static string ClientID = "<ENTER CLIENT/APPLICATION ID>";
+
+        // The Azure AD App - Client Secret
+        public static string ClientSecret = "<ENTER CLIENT SECRET>";
+    }
+}
