747899c45cc0edd58092009ea37079e6d3064656
Ernesto Cianciotta <ernesto.cianciotta@microsoft.com>
2025-04-11 12:24:59 +0200

    Aggiunto test controller per debugging.

---
+++ AppOwnsData/Controllers/TestsController.cs
@@ -0,0 +1,32 @@
+﻿using AppOwnsData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AppOwnsData.Controllers
+{
+    public class TestsController(IOptions<AzureAd> azureAd) : Controller
+    {
+
+        [HttpPost]
+        public IActionResult ServicePrincipalAuthTest()
+        {
+            string tenantId = azureAd.Value.TenantId;
+            string url = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token";
+            HttpClient client = new HttpClient();
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, url);
+            StringContent content = new StringContent($"client_id={azureAd.Value.ClientId}&client_secret={azureAd.Value.ClientSecret}&scope={azureAd.Value.ScopeBase[0]}&grant_type=client_credentials", System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+            message.Content = content;
+            HttpResponseMessage response = client.Send(message);
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return BadRequest("Service Principal authentication failed.");
+            }
+            if (string.IsNullOrEmpty(responseBody))
+            {
+                return BadRequest("Service Principal authentication failed.");
+            }
+            return Content(responseBody, "application/json");
+        }
+    }
+}
